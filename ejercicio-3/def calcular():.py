cc3848a198e660f51eb3255a372ec7a2a64e30af
Mauricio <m4uytw@gmail.com>
2026-08-14 18:06:27 -0300

    Alojamientos

---
+++ ejercicio-3/def calcular():.py
@@ -0,0 +1,17 @@
+pasaje = int(input("Costo pasaje: "))
+alojamiento = int(input("Costo alojamiento: "))
+noches = int(input("Cantidad de noches: "))
+dinero = int(input("Dinero disponible: "))
+total = alojamiento * noches    
+total+= pasaje
+if dinero >= total:
+   logico = True
+else:
+   logico = False
+
+if logico:
+   print(f"Te alcanza re bien crack")
+else:
+   print(f"Segui participando")
+
+    

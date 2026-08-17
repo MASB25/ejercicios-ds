17f6729b5e4b41d53fe6ac9d92421776570a6ed4
Mauricio <m4uytw@gmail.com>
2026-08-17 01:24:44 -0300

    contraseña

---
+++ ejercicio-5/Untitled-1.py
@@ -0,0 +1,32 @@
+if __name__ == "__main__":
+    booleano = True
+    while booleano:
+        contraseña = input("Ingresa una contraseña: ")
+        if len(contraseña) < 8:
+            print("Muy corta maestro")
+        elif contraseña.isupper():
+            print("Te comiste la mayuscula mi rey")
+        elif contraseña.islower():
+            print("Te comiste la minuscula mi rey")
+        else:
+            print("Te juro que no vi tu constraseña, pero confio")
+            booleano = False
+            cantidad = 0
+    while cantidad < 3:
+        try:
+            repeticion = input("A ver si te acordas tu contraseña, escribila: ")
+            if repeticion == contraseña:
+                print("Sos crack")
+                cantidad = 4
+            else:
+                print("Mepa que esa no era, floja esa memoria...")
+                cantidad += 1
+            if cantidad == 3:
+                raise ValueError("Te pasaste de intentos, a laburar esa memoria")
+            
+        except ValueError as e:
+            print(f"Error {e}")
+
+
+    
+    
